380c656b59f0166c4747470d453e6d489f26cd88
cuong <Vuc7997@gmail.com>
2021-04-01 20:01:17 +0700

    init

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import {  StyleSheet, Text, View,TextInput,TouchableHighlight,CheckBox } from "react-native";
+
+const App = () => (
+  <View style={[styles.container]}>
+    <View style={{alignItems: "center",}}>
+      <Text>SIGN IN</Text>
+      </View>
+      <View >
+        <Text>Nhap  tai khoan</Text>
+      <TextInput style={styles.input}
+        //keyboardType='email-address'
+        //placeholder='ente your email'
+        //placeholderTextColor='red'
+       /> 
+       <Text>Nhap  tai khoan</Text>
+      <TextInput style={styles.input}  />
+       <TouchableHighlight style={styles.hi}>
+        <View style={styles.button}>
+          <Text>Đăng Nhập</Text>
+        </View>
+      </TouchableHighlight>
+      </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },  
+  input:{
+    height:40,
+         margin: 20,
+         borderColor:'gray',
+         borderWidth:1,
+         padding:10
+  },
+  hi:{
+    alignItems: "center",
+  },
+  button:{
+    width:200,
+    alignItems: "center",
+    backgroundColor: "red",
+    padding: 10,
+    borderRadius:50,
+   },
+});
+
+export default App;
